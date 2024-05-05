41f709314fa729a7790dab6cde3efdca0baba5f3
nxtbn <bytenyxlimited@gmail.com>
2024-05-05 12:42:03 +0600

    handled jwt token

--- nxtbn/users/authentication.py
+++ nxtbn/users/authentication.py
@@ -1,5 +1,7 @@
 from rest_framework.authentication import SessionAuthentication
-
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+from utils.jwt_utils import verify_jwt_token
 class CsrfExemptSessionAuthentication(SessionAuthentication):
     '''
         Security Warning: Don't use this in production environment.
@@ -9,3 +11,16 @@ class CsrfExemptSessionAuthentication(SessionAuthentication):
     '''
     def enforce_csrf(self, request):
         pass
+
+
+
+class JWTAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        auth_header = request.headers.get("Authorization")
+        if auth_header and auth_header.startswith("Bearer "):
+            token = auth_header.split(" ")[1]
+            user = verify_jwt_token(token)
+            if user:
+                return (user, None)
+            raise AuthenticationFailed("Invalid or expired token")
+        return None
