ef5a82cf41b5bb95cee6d5920f0c89f306c24723
nxtbn <bytenyxlimited@gmail.com>
2024-05-06 15:46:55 +0600

    docs redirection

--- nxtbn/home/views.py
+++ nxtbn/home/views.py
@@ -3,22 +3,10 @@ from django.urls import reverse
 
 from django.http import HttpResponse
 
-# Create a view to serve robots.txt
-def robots_txt(request):
-    # Create the content for robots.txt
-    content = (
-        "User-agent: *\n"
-        "Disallow: /docs/\n" 
-        "Disallow: /admin/\n"
-        "Disallow: /api/\n"
-        "Allow: /\n"  # Allow everything else
-    )
-    
-    return HttpResponse(content, content_type="text/plain")
-
 
 def home(request):
-    if request.user.is_authenticated:
-        return redirect(reverse('api_playground'))
-    else:
-        return redirect(reverse('account_login'))
+    return redirect(reverse('api_docs'))
+    # if request.user.is_authenticated:
+    #     return redirect(reverse('api_playground'))
+    # else:
+    #     return redirect(reverse('account_login'))
