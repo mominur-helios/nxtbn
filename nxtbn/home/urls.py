8e23ef88f9bd141ef48f6d460b3eac4c181763df
nxtbn <bytenyxlimited@gmail.com>
2024-05-06 20:16:46 +0600

    sitemap xml

--- nxtbn/home/urls.py
+++ nxtbn/home/urls.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from django.contrib.sitemaps.views import sitemap
+
 
 from nxtbn.home import views as home_views
 from nxtbn.seo import views as seo_views
@@ -8,5 +10,6 @@ from nxtbn.seo import views as seo_views
 
 urlpatterns = [
     path('', home_views.home, name='home'),
-    path("robots.txt", seo_views.robots_txt, name="robots_txt"), 
+    path("robots.txt", seo_views.robots_txt, name="robots_txt"),
+    path("sitemap.xml", sitemap, {"sitemaps": seo_views.site_maps}, name="sitemap_xml"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
