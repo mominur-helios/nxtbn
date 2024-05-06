2aca3e6a8e9faa25160eb484b3f20f9d85df324a
nxtbn <bytenyxlimited@gmail.com>
2024-05-06 15:38:39 +0600

    added robots.txt

--- nxtbn/home/urls.py
+++ nxtbn/home/urls.py
@@ -7,4 +7,5 @@ from . import views as home_views
 
 urlpatterns = [
     path('', home_views.home, name='home'),
+    path("robots.txt", home_views.robots_txt, name="robots_txt"), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
