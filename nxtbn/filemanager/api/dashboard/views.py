4ca61b31c49a594bbc1455e3b20efd4b3d16f8f7
hasib-helios <hasib@heliosinsider.com>
2024-05-07 11:04:34 +0600

    feat: implemented file manager dashboard api

--- nxtbn/filemanager/api/dashboard/views.py
+++ nxtbn/filemanager/api/dashboard/views.py
@@ -1,5 +1,41 @@
-from rest_framework import generics, status
-from rest_framework.response import Response
+from rest_framework import generics
 from django.utils.translation import gettext_lazy as _
-from rest_framework.permissions  import AllowAny
-from rest_framework.exceptions import APIException
+
+
+from nxtbn.filemanager.models import Document, Image
+from nxtbn.filemanager.api.dashboard.serializers import (
+    DocumentSerializer,
+    ImageSerializer,
+)
+from nxtbn.core.admin_permissions import NxtbnAdminPermission
+from nxtbn.core.paginator import NxtbnPagination
+
+
+class ImageListView(generics.ListCreateAPIView):
+    serializer_class = ImageSerializer
+    queryset = Image.objects.all()
+    pagination_class = NxtbnPagination
+    permission_classes = (NxtbnAdminPermission,)
+
+
+class ImageDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
+    pagination_class = NxtbnPagination
+    permission_classes = (NxtbnAdminPermission,)
+    lookup_field = "id"
+
+
+class DocumentListView(generics.ListCreateAPIView):
+    serializer_class = DocumentSerializer
+    queryset = Document.objects.all()
+    permission_classes = (NxtbnAdminPermission,)
+    pagination_class = NxtbnPagination
+
+
+class DocumentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
+    pagination_class = NxtbnPagination
+    permission_classes = (NxtbnAdminPermission,)
+    lookup_field = "id"
