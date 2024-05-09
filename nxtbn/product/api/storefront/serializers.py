157f39903090d741bf533a133ed87117f1688172
nxtbn <bytenyxlimited@gmail.com>
2024-05-09 16:14:46 +0600

    added filter

--- nxtbn/product/api/storefront/serializers.py
+++ nxtbn/product/api/storefront/serializers.py
@@ -1,3 +1,47 @@
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 from django.db import transaction
+
+from nxtbn.product.api.dashboard.serializers import RecursiveCategorySerializer
+from nxtbn.product.models import Product, Collection, Category
+
+class CategorySerializer(RecursiveCategorySerializer):
+    pass
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ('id', 'name', 'description', 'is_active', 'image',)
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            'id',
+            'name',
+            'summary',
+            'description',
+            'category',
+            'brand',
+            'type',
+            'currency',
+        )
+
+
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            'id',
+            'name',
+            'summary',
+            'description',
+            'brand',
+            'type',
+            'currency',
+            'category',
+            'collections',
+            'media',
+            'created_by',
+        )
